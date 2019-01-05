60e6b968f4ce8b4ba0efe7c6a86a0983646bfa1a
Jesse Ling <jesse.ryan.ling@gmail.com>
2019-01-05 16:03:48 -0700

    Navbar skeleton in place

---
+++ spec/features/visitor_visits_index_page_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  context 'they visit /dogs' do
+    it 'displays a navbar with option to login and sign up' do
+      visit '/dogs'
+      save_and_open_page
+      within '.navbar' do
+        expect(page).to have_content('Login')
+      end
+    end
+  end
+end
