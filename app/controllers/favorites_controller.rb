74ce88ec91e0fcd871da6de25824c29007ecf2a3
Jesse Ling <jesse.ryan.ling@gmail.com>
2019-01-06 21:57:13 -0700

    Create favorites model

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,10 @@
+class FavoritesController < ApplicationController
+  def create
+    @favorite = Favorite.new(url: params[:url], user_id: current_user.id)
+    if @favorite.save
+      flash[:success] = "Favorite Dog Added"
+      binding.pry
+      redirect_to dogs_path
+    end
+  end
+end
