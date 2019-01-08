52701aad5b9ba6a189a0d19af5caef57960a87eb
Jesse Ling <jesse.ryan.ling@gmail.com>
2019-01-08 14:41:09 -0700

    Add test

---
+++ spec/features/user_saves_a_dog_pic_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'As a logged in user' do
+  context 'they visit /dogs and click on Save' do
+    it 'saves their favorite photo and redirects to /favorites' do
+      user = FactoryBot.create(:user)
+      login_as(user, :scope => :user)
+
+      visit '/'
+
+      first('.card-body').click_link('Save')
+
+      expect(current_path).to eq '/favorites'
+    end
+  end
+end
